55386f8a3d0dba2133a1e759a878b7b87b0e5c59
Fess194100 <frilanser194100@yandex.ru>
2025-05-15 18:49:11 +0300

    Add Models: PresurePlate, Vase.

--- HollowAntarctica/Assets/_ProjectAntarctica/Player/Scripts/SimpleEnums.cs
+++ HollowAntarctica/Assets/_ProjectAntarctica/Player/Scripts/SimpleEnums.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace SimpleCharController
@@ -8,4 +9,10 @@ namespace SimpleCharController
         ropeLadder,
     }
 
+    public enum TypePortableObject
+    {
+        None,
+        Curcle_0, Curcle_1, Curcle_2, Curcle_3, Curcle_4, Curcle_5, Curcle_6, Curcle_7, Curcle_8, Curcle_9,
+        Box_0, Box_1, Box_2, Box_3, Box_4, Box_5, Box_6, Box_7, Box_8, Box_9,
+    }
 }
---
+++ HollowAntarctica/Assets/_ProjectAntarctica/Scripts/Tools/HDRColorForFSM.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class HDRColorForFSM : MonoBehaviour
+{
+    [ColorUsage(true, true)]
+    public Color HDRColor;
+}
